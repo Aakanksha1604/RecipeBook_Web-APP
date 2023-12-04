afbb0608744ef463bb068260b5d271282b0d97ec
Aakanksha-04 <Aakanksham52@gmail.com>
2023-12-04 11:07:01 +0530

    first commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,31 @@
+import { useState } from "react";
 import FryingPan from "./FryingPan";
 import Recipe from "./Recipe";
 import Spinner from "./Spinner";
 
 const Home = ({ recipes, loading, error }) => {
+  const[inputValue,setInputValue]=useState('');
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    setInputValue(value);
+  }
+  
   return (
     <div className="home container mx-auto py-10 flex flex-wrap gap-10 justify-center">
       {/* condition for ideal position */}
       {!loading && !error && recipes.length === 0 ? (
         <div>
-          <p className="text-2xl lg:text-4xl font-semibold text-rose-300">
-            Nothing to show, please search anything!
-          </p>
+          {/* <p className="text-2xl lg:text-4xl font-semibold text-rose-300">
+            Add Your Recipe
+          </p> */}
+          <label htmlFor="inputBox">Add </label>
+          <input
+          type="text"
+          placeholder="add your recipe..."
+          id="inputBox"
+          value={inputValue}
+          onChange={handleInputChange}
+          />
           <FryingPan />
         </div>
       ) : null}
